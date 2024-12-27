7875aabb8e56217034f93c7ed91da7ba763366f3
TieuHoangDat <DatTH.B21CN220@stu.ptit.edu.vn>
2024-12-27 07:47:35 +0700

    cam

--- src/pages/Cam.jsx
+++ src/pages/Cam.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 const Introduce = () => {
   const [imageUrl, setImageUrl] = useState(
-    "https://5a62-2401-d800-f22f-d03e-4d72-e1c5-d248-1cb3.ngrok-free.app/cam-lo.jpg"
+    "https://76f1-2402-800-9aa8-25ed-40fc-2d72-b31c-3a53.ngrok-free.app/cam-lo.jpg"
   );
 
   useEffect(() => {
     const interval = setInterval(() => {
       setImageUrl((prevImage) => {
         // Mỗi lần thay đổi, sẽ load lại URL ảnh mới
-        return `https://5a62-2401-d800-f22f-d03e-4d72-e1c5-d248-1cb3.ngrok-free.app/cam-lo.jpg?${new Date().getTime()}`;
+        return `https://76f1-2402-800-9aa8-25ed-40fc-2d72-b31c-3a53.ngrok-free.app/cam-lo.jpg?${new Date().getTime()}`;
       });
     }, 1000); // 1 giây để tải lại hình ảnh mới
 
